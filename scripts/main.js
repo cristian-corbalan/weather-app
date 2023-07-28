51e4f6401d4ce6d2a148d5100b297325150c8508
cristian-corbalan <ccorbalandw@gmail.com>
2023-07-27 22:39:28 -0300

    Change the temperature from Kelvin to Celsius.

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 
 const API_KEY = "1bff7579067727b7399aa930fbeb7b4c";
 
-const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${API_KEY}`;
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid=${API_KEY}`;
 
 const form = document.querySelector('form');
 const content = document.getElementById('content');
